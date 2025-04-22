7c9a35dbc69f7a51e16a3510c4eb903020804557
VeskoDev <lazarevic.vesko@gmail.com>
2025-04-22 12:13:09 +0200

    Initial Commit

---
+++ qzzy_app_final/qzzy_app_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  qzzy_app_finalApp.swift
+//  qzzy_app_final
+//
+//  Created by Veselin Lazarevic on 22.4.25..
+//
+
+import SwiftUI
+
+@main
+struct qzzy_app_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
